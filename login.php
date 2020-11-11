63160c6e047249478780beb9e159cf41e814bc61
angelicaarellanes <angelica.arellanes@beyondexperience.com>
2020-11-11 10:28:50 -0500

    Front inicio de sesión

---
+++ login.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+<title>.: Caritas</title>
+
+<!-- bootstrap 4 vue.js -->
+<?php
+include("header.php");
+?>
+
+<link href="_assets/_css/login.css?x=<?php echo(rand()); ?>" type="text/css" rel="stylesheet" />
+
+
+<div class="sidenav">
+         <div class="login-main-text text-login">
+         <h2> Caritas, Q Roo. </h2>
+
+            
+            <p>
+              Seguimos trabajando por los que menos tienen, a través del banco de alimentos.
+               Por ello siguiendo las medidas de prevención de las autoridades y con las donaciones de nuestros aliados y
+                gente de buen corazón seguimos comprometidos con ayudar a quien más lo necesita.
+            </p>
+         </div>
+         <div>
+
+</div>
+      </div>
+      <div class="main">
+         <div class="col-md-6 col-sm-12">
+            <div class="login-form">
+               <h3> JUNTOS PARA AYUDAR A QUIEN MÁS LO NECESITA   </h3>
+               <form>
+                  <div class="form-group">
+                      <input type="text" class="form-control" placeholder="User Name">
+                  </div>
+                  <div class="form-group">
+                     <input type="password" class="form-control" placeholder="Password">
+                  </div>
+                   <button type="submit" class="btn btn-black">Login</button> 
+                  
+                  
+               </form>
+            </div>
+         </div>
+      </div>
+</html>
